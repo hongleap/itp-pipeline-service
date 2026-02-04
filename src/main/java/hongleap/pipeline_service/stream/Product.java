8cf68b8054d9ac5f4d12a5768a571a09f63c1c15
hongleap <hongleap0000@gmail.com>
2026-02-05 00:26:45 +0700

    Create Product entity

---
+++ src/main/java/hongleap/pipeline_service/stream/Product.java
@@ -0,0 +1,13 @@
+package hongleap.pipeline_service.stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String code;
+    private Integer qty;
+}
